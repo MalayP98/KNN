d592e61167e809103d0ce041333a71f2d7134528
malay <tomalaypandey.tanu@gmail.com>
2019-11-10 14:15:00 +0530

    KNN Class structure

--- knn.py
+++ knn.py
@@ -33,11 +33,12 @@ def confusion_matrix(input, target):
                 flase_negative += 1
             else:
                 flase_positive += 1
-    print("Confusion Matrix: ", np.array([[true_positive,flase_positive],[flase_negative, true_negative]]))
+    print("Confusion Matrix: ", np.array([[true_positive, flase_positive], [flase_negative, true_negative]]))
     print("True Positive = ", true_positive)
     print("False Positive = ", flase_positive)
     print("True Negative = ", true_negative)
     print("False Negative = ", flase_negative)
+
 
 def train_test_split(input, target, ratio):
     shuffle_indices = np.random.permutation(input.shape[0])
@@ -74,3 +75,28 @@ class FeatureScaling:
                 self.data[:, i] = (self.data[:, i] - np.mean(self.data[:, i])) / (
                         max(self.data[:, i]) - min(self.data[:, i]))
             return self.data
+
+
+class NearestNeighbourError(Exception):
+    pass
+
+
+class KNN:
+    def __init__(self, neighbour, norm='L2'):
+        self.neighbour = neighbour
+        self.norm = norm
+
+    def distance(self):
+        pass
+
+    def extract_norm(self):
+        pass
+
+    def nearest_neighbour_output(self):
+        pass
+
+    def majority_class(self):
+        pass
+
+    def predict(self):
+        pass
